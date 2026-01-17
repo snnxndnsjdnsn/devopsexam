52d364405276e5c0e506a151c3b71c1fc834cde8
snnxndnsjdnsn <samarthmjoshi999@gmail.com>
2026-01-17 13:30:21 +0530

    3 commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sys
 if len(sys.argv)==2:
     text=sys.argv[1]
     print("the text is :",text)
-else:
     res=text[::-1]
     print("the reversed string is :",res)
+else:
+    print("the defult values can be take")
